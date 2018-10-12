4dc06394f657c0e19a5b1981d238d6c0c00766b2
vladonmsk <v-and@bk.ru>
2018-10-12 16:42:09 +0300

    third commit

--- js/script.js
+++ js/script.js
@@ -5,4 +5,57 @@ document.body.onload = function () {
 		preloader.className = 'done';
 		map.className = 'maps';
 	}, 1000);
-}
+    var hour = 0;
+    var minute = 0;
+    var second = 0;
+	/*setTimeout(function () {
+		var timer = document.getElementById('timer');
+		second += 1;
+		if (second == 60){
+			minute += 1;
+			second = 0;
+		}
+		if(minute == 60){
+			hour += 1;
+			minute = 0;
+		}
+		timer.innerHTML = hour + " часов " + minute + " минут " + second + " секунд";
+    }, 1000);*/
+
+
+	function TimerClock() {
+        var timer = document.getElementById('timer');
+        second ++;
+        if (second == 60){
+            minute ++;
+            second = 0;
+        }
+        if(minute == 60){
+            hour ++;
+            minute = 0;
+        }
+        timer.innerHTML = "0" + hour + " часов " + "0" + minute + " минут " + "0" + second + " секунд";
+    }
+    //while(true){
+    setTimeout(TimerClock, 1000);
+	//}
+
+    $('#rezume').on('click', function (e) {
+        e.preventDefault();
+        $('#content_rezume').show();
+        $('#content_map').hide();
+        $('#content_timer').hide();
+    });
+    $('#mapa').on('click', function (e) {
+        e.preventDefault();
+        $('#content_rezume').hide();
+        $('#content_map').show();
+        $('#content_timer').hide();
+    });
+    $('#timer1').on('click', function (e) {
+        e.preventDefault();
+        $('#content_rezume').hide();
+        $('#content_map').hide();
+        $('#content_timer').show();
+    })
+};
